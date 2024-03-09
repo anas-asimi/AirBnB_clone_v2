4763bfcd7ddf3f545641da26bff84f4f8cf2b311
Anas Asimi <anas2020cr7@gmail.com>
2024-03-10 00:27:54 +0100

    alx task

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ from fabric.api import env
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["54.160.85.72", "35.175.132.106"]
+env.hosts = ["18.206.198.88", "100.24.255.186"]
 
 
 def do_deploy(archive_path):
