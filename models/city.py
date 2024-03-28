64cf929e94c9a3c78e048f9aad0c3b834dc9c7cd
Anas Asimi <anas2020cr7@gmail.com>
2024-03-28 00:07:59 +0000

    update Class City

--- models/city.py
+++ models/city.py
@@ -1,17 +1,24 @@
-#!/usr/bin/python3
-""" City Module for HBNB project """
-from models import storage_type
-from sqlalchemy import Column, String, ForeignKey
+#!/usr/bin/python
+""" Class City"""
+import models
 from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
-    if storage_type == "db":
+    """Representation of city """
+    if models.storage_type == "db":
         __tablename__ = 'cities'
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="cities")
     else:
         state_id = ""
         name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes city"""
+        super().__init__(*args, **kwargs)
